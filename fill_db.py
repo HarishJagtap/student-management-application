3504b007a4774b3566170b8c4f57857aa7a3f06e
Harish Jagtap <harish@quichub.com>
2022-05-15 23:02:27 +0530

    Added script to fill db

--- fill_db.py
+++ fill_db.py
@@ -7,6 +7,14 @@ def fill_books():
     with open("books.csv", newline="") as f:
         csvfile = csv.DictReader(f)
         for row in csvfile:
+            date_ = row["publication_date"]
+            if date_:
+                date_ = date_.split("-")
+                date_.reverse()
+                date_ = "-".join(date_)
+                row["publication_date"] = date_
+            else:
+                row.pop("publication_date")
             book = Book(**row)
             book.save()
 
@@ -21,8 +29,18 @@ def fill_students():
     with open("students.csv", newline="") as f:
         csvfile = csv.DictReader(f)
         for row in csvfile:
+            if row["school"]:
+                row["school"] = School.objects.get(name=row["school"])
+            else:
+                row["school"] = None
+            book = None
+            if row["books"]:
+                book = Book.objects.get(title=row["books"])
+            row.pop("books")
             student = Student(**row)
             student.save()
+            if book:
+                student.books.add(book)
 
 def main():
     fill_books()
